5ca5f2bbe7aafaddb40a28d37774e5583b8010ee
Bartosz Osowski <bartosz.osowski@errigal.com>
2018-11-20 20:51:06 +0000

    Fixed a building issue. Sorted classes into groups.

---
+++ AssetManagerv1/GameObject.cpp
@@ -0,0 +1,9 @@
+//
+//  GameObject.cpp
+//  AssetManagerv1
+//
+//  Created by Bartosz Osowski on 20/11/2018.
+//  Copyright © 2018 Denis Flynn. All rights reserved.
+//
+
+#include "GameObject.hpp"
---
+++ AssetManagerv1/GameObject.hpp
@@ -0,0 +1,17 @@
+//
+//  GameObject.hpp
+//  AssetManagerv1
+//
+//  Created by Bartosz Osowski on 20/11/2018.
+//  Copyright © 2018 Denis Flynn. All rights reserved.
+//
+
+#ifndef GameObject_hpp
+#define GameObject_hpp
+
+#include <stdio.h>
+
+class GameObject{
+    
+};
+#endif /* GameObject_hpp */
